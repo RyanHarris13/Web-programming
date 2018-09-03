330b7ade99d8e901bc76063cf6ea054524a33bd2
RyanHarris13 <s3719229@student.rmit.edu.au>
2018-09-03 13:10:31 +1000

    added products page, copy of index

---
+++ a2/ProductsPage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <title>Assignment 2</title>
+    
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+
+    <header>
+      <div><img src='../../media/exit-and-bond-cleaning-wantirna-south-3152-logo.gif' alt='ExitBond&Cleaning' />
+          <p>Exit & bond cleaning.</p></div>
+    </header>
+
+    <nav>
+      <div>Navigation:
+          
+        <a href="https://titan.csit.rmit.edu.au/~s3717184/wp/">Caleb's index page</a>
+          <br>
+        <a href="https://titan.csit.rmit.edu.au/~s3719229/wp/">Ryan's index page</a>
+        </div>
+        
+    </nav>
+
+    <main>
+      <article id='Website Under Construction'>
+    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+        <img src='../../media/' alt='' />
+      </article>
+    </main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+      </script> By: Caleb McCash (s3717184) and Ryan Harris(s3719229). Group name: A2-s3719229-s3717184 10</div>
+        
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+  </body>
+</html>
