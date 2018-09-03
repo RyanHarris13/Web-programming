2579ae8ce07bcf3be898f2dbcae23536eea3839b
RyanHarris13 <s3719229@student.rmit.edu.au>
2018-09-03 13:08:21 +1000

    just prelim stuff

--- a2/index.php
+++ a2/index.php
@@ -13,24 +13,32 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div><img src='../../media/exit-and-bond-cleaning-wantirna-south-3152-logo.gif' alt='ExitBond&Cleaning' />
+          <p>Exit & bond cleaning.</p></div>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <div>Navigation:
+          
+        <a href="https://titan.csit.rmit.edu.au/~s3717184/wp/">Caleb's index page</a>
+          <br>
+        <a href="https://titan.csit.rmit.edu.au/~s3719229/wp/">Ryan's index page</a>
+        </div>
+        
     </nav>
 
     <main>
       <article id='Website Under Construction'>
     <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+        <img src='../../media/' alt='' />
       </article>
     </main>
 
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here.</div>
+      </script> By: Caleb McCash (s3717184) and Ryan Harris(s3719229). Group name: A2-s3719229-s3717184 10</div>
+        
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
